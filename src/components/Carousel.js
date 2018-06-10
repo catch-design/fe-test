bb59e041b0cc357ffb8fbbdf14e76db6892cfa01
Ryan Achten <ryanachten@gmail.com>
2018-06-10 14:46:19 +1200

    Add carousel component styles

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default () => (
-  <section>
-    <img src="http://via.placeholder.com/236x174" alt="Mountainside"/>
-    <img src="http://via.placeholder.com/236x174" alt="Mountain"/>
-    <img src="http://via.placeholder.com/236x174" alt="Beach"/>
-    <img src="http://via.placeholder.com/236x174" alt="Great view"/>
+  <section className="c-carousel__container">
+    <img className="c-carousel__item"
+      src="http://via.placeholder.com/236x174" alt="Mountainside"/>
+    <img className="c-carousel__item"
+      src="http://via.placeholder.com/236x174" alt="Mountain"/>
+    <img className="c-carousel__item"
+      src="http://via.placeholder.com/236x174" alt="Beach"/>
+    <img className="c-carousel__item"
+      src="http://via.placeholder.com/236x174" alt="Great view"/>
   </section>
 );
