f83461abe00d9ab8fd6e4773a542eb1edeb680e9
Ryan Achten <ryanachten@gmail.com>
2018-06-10 14:25:26 +1200

    Add form component styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 export default () => (
   <form role="form">
-    <h2>Form title</h2>
+    <h2 className="c-form__title">Form title</h2>
     <div className="o-layout">
       <input type="text" placeholder="First name" aria-label="First name"
-        className="o-layout__item" />
+        className="o-layout__item c-form__input" />
       <input type="text" placeholder="Last name" aria-label="Last name"
-        className="o-layout__item" />
+        className="o-layout__item c-form__input" />
       <input type="text" placeholder="Email address" aria-label="Email address"
-        className="o-layout__item" />
+        className="o-layout__item c-form__input" />
       <select name="city" aria-label="City" defaultValue=""
-        className="o-layout__item">
+        className="o-layout__item c-form__input c-form__select">
         <option value="" disabled>City</option>
         <option value="kingston">Kingston</option>
         <option value="montego-bay">Montego Bay</option>
         <option value="ocho-rios">Ocho Rios</option>
       </select>
     </div>
-    <button>Submit</button>
+    <button className="c-form__submit">Submit</button>
   </form>
 );
